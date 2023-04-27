7f9e7283747654e8f1e3c0fe8d789e5740da0110
StefanTheSpider <stefantuaevgmail.com>
2023-04-27 16:39:03 +0200

    ein commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static('public'))
 
-app.get('/', function(req, res) {
+/* app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
+}); */
+
+app.get('/', function(req, res) {
+  let today = new Date();
+  let currentDay = { weekday: 'long' }
+  let day = today.toLocaleDateString('de-DE', currentDay);
+
+  res.render('list', {kindOfDay: day});
 });
 
 app.listen(3000, () => console.log('Server is runing on port 3000'));
