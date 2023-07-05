6e5c2404adcedafcaacae6d46f4d658006f26a41
StefanTheSpider <stefantuaevgmail.com>
2023-07-05 14:22:56 +0200

    initial commit v1.2

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const getDate = require('./date');
-// Ruft das modul in der 'date.js' Datei auf
 const date = require(__dirname + '/date.js');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,7 +17,14 @@ const itemsSchema = new mongoose.Schema ({
 
 const Item = mongoose.model('Item', itemsSchema, 'item');
 
+//*! Hauptteil */  
+
 const addedItems = [];
+
+app.get('/', function(req, res) {
+  let day = date.getDate();
+  res.render('list.ejs', {ListTitle: day, newListItems: addedItems});
+});
 
 async function itemsCall() {
   try {
@@ -32,78 +37,28 @@ async function itemsCall() {
   }
 };
 
-async function itemsDelete() {
-  try {
-    const items = await Item.findOneAndDelete();
-    items.forEach((item) => {
-      addedItems.push(item.name);
-    })
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-app.get('/', function(req, res) {
-  // wir nuzen die Funktion aus der 'date.js' Datei
-  let day = date.getDate();
-  // hier erzeugen wir die Variablen, die wir dann ins HTML der entsprechenden Datei injezieren
-  res.render('list.ejs', {ListTitle: day, newListItems: addedItems});
-});
-
 app.post('/', function(req, res) {
   const itemName = req.body.newItem;
-
   const item = new Item ({
     name: itemName
   })
    item.save();
-   addedItems.push(item.name)
+   addedItems.push(item)
    res.redirect('/');
   })
   itemsCall();
   
-  app.post('/delete', (req, res) => {
-    const checkedItem = req.body.checkbox;
-    console.log(checkedItem);
-    //res.redirect('/');
+  app.post("/delete", async function(req, res){
+    try {
+      const checkedItemId = req.body.checkbox;
+      console.log(checkedItemId);
+      if(checkedItemId !== undefined){
+        await Item.findByIdAndRemove(checkedItemId);
+        console.log(`Deleted ${checkedItemId} Successfully`);
+        res.redirect('/')
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
   });
-
-/*
-const coocking = new Item ({
-  neme: 'coocking'
-});
-const gaming = new Item ({
-  name: 'gaming'
-});
-const reading = new Item ({
-  name: 'reading'
-});
-async function insert() {
-  try {
-    Item.insertMany([coocking, gaming, reading])
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-insert()
-*/
-
-
-/*
-const workItems = [];
-const items = [];
-app.post('/', function(req, res) {
-  let item = req.body.newItem;
-  if (req.body.list === 'Work') {
-    workItems.push(item);
-  res.redirect('/work')
-  } else {
-    items.push(item);
-    res.redirect('/');
-  }
-});
-*/
-
-
 app.listen(3000, () => console.log('Server is runing on port 3000'));
